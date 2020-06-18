f284a6bb6a51753d5f65a1fd2558fd6beab59195
keely_lee <keely_lee@outlook.com>
2020-06-18 01:07:58 -0400

    Add design to recipe create

--- frontend/src/components/recipe/recipe_create.jsx
+++ frontend/src/components/recipe/recipe_create.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './recipe_create.css'
+import './recipe_create.css';
+// import herbs from './create-herbs.png';
 
 class RecipeCreate extends React.Component {
   constructor(props) {
@@ -9,7 +10,14 @@ class RecipeCreate extends React.Component {
       servings: "",
       ingredients: [],
       instructions: "",
-      inputs: ["input-0"],
+      inputs: [
+        "input-0",
+        "input-1",
+        "input-2",
+        "input-3",
+        "input-4",
+        "input-5",
+      ],
       test: "",
     };
 
@@ -44,21 +52,20 @@ class RecipeCreate extends React.Component {
       <div className="recipe-create-div">
         <div className="recipe-create-div-wrap">
           <form onSubmit={this.addInput()} className="ingredient-outer-form">
+            {/* <img src={herbs} alt="" draggable="false" id="ingredients-img"/> */}
             <div className="recipe-ingredients-div">
               <label className="recipe-ingredients">
                 Ingredients:
-                <div className="ingredient-input-div">
-                  {this.state.inputs.map((input, idx) => (
-                    <input
-                      key={input}
-                      type="text"
-                      onChange={this.update("test")}
-                      className={`ingredient-num-${idx}`}
-                    />
-                  ))}
-                </div>
+                <button id="add-ingredient-btn">+</button>
+                {this.state.inputs.map((input, idx) => (
+                  <input
+                    key={input}
+                    type="text"
+                    onChange={this.update("test")}
+                    className={`ingredient-num-${idx}`}
+                  />
+                ))}
               </label>
-              <button>Add</button>
             </div>
           </form>
 
