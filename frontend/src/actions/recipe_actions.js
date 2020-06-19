907627c48f03548a61edea2df70798ebbc8356da
Daniel Gorter <danieljgorter@gmail.com>
2020-06-19 00:15:19 -0400

    stock photos

--- frontend/src/actions/recipe_actions.js
+++ frontend/src/actions/recipe_actions.js
@@ -1,5 +1,6 @@
 import * as RecipeAPIUtil from '../util/recipe_api_util';
 import { receiveErrors } from './session_actions';
+import baconPie from '../bacon-pie.jpeg'
 
 export const RECEIVE_RECIPE = "RECEIVE_RECIPE";
 export const RECEIVE_CURRENT_RECIPE = "RECEIVE_CURRENT_RECIPE";
@@ -38,21 +39,24 @@ export const clearRecipes = () => ({
 
 export const fetchRecipe = recipeId => dispatch => {
   return RecipeAPIUtil.fetchRecipe(recipeId)
-    .then(recipe => dispatch(receiveRecipe(recipe.data)), 
+    .then(recipe => verifyRecipePhoto(recipe.data))
+    .then(recipe => dispatch(receiveRecipe(recipe)), 
       err => dispatch(receiveErrors(err.response.data)));
 }
 
 export const fetchRecipes = query => dispatch => {
   return RecipeAPIUtil.fetchRecipes(query)
+    .then(recipes => verifyAllRecipePhotos(recipes.data))
     .then(recipes => { 
-      dispatch(receiveRecipes(recipes.data)) })
+      dispatch(receiveRecipes(recipes)) })
     .catch(err => dispatch(receiveErrors(err.response.data)));
 }
 
 export const fetchPinnedRecipes = (userId) => (dispatch) => {
   return RecipeAPIUtil.fetchPinnedRecipes(userId)
+    .then(recipes => verifyAllRecipePhotos(recipes.data))
     .then((recipes) => {
-      dispatch(receiveRecipes(recipes.data));
+      dispatch(receiveRecipes(recipes));
     })
     .catch((err) => dispatch(receiveErrors(err.response.data)));
 };
@@ -60,15 +64,51 @@ export const fetchPinnedRecipes = (userId) => (dispatch) => {
 export const fetchOwnRecipes = (userId) => (dispatch) => {
   // debugger
   return RecipeAPIUtil.fetchOwnRecipes(userId)
+    .then(recipes => verifyAllRecipePhotos(recipes.data))
     .then((recipes) => {
-      dispatch(receiveRecipes(recipes.data));
+      dispatch(receiveRecipes(recipes));
     })
     .catch((err) => dispatch(receiveErrors(err.response.data)));
 };
+
 export const createNewRecipe = recipe => dispatch => {
   return RecipeAPIUtil.createRecipe(recipe)
+    .then(recipe => verifyRecipePhoto(recipe.data))
     .then(recipe => {
       dispatch(receiveRecipe(recipe))}) 
     .catch((err) => dispatch(receiveNewRecipeErrors(err.response.data)));
+}
+
+async function verifyRecipePhoto(data) {
+  return verifyPhoto(data.recipe.image)
+      .then( newUrl => data.recipe.image = newUrl )
+      .then( () => data );
+}
+
+async function verifyAllRecipePhotos(recipes) {
+  let verifyPromises = recipes.map( recipe => {
+    return verifyPhoto(recipe.image)
+      .then( newUrl => recipe.image = newUrl );
+  })
+
+  return Promise.all(verifyPromises).then( () => recipes);
+}
+
+async function verifyPhoto(url){
+  if (!url) { return baconPie };
+  
+  const img = new Image();
+
+  const checkDimensions = new Promise( (resolve, reject) => {
+    img.onload = () => {
+      resolve( img.height > 10 && img.width > 10);
+    }
+    img.onerror = () => reject();
+    img.src = url;
+  })
+
+  let isValid = await checkDimensions;
+  
+  return isValid ? url : baconPie;
 }
 
